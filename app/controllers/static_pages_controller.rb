1ce822342d3bf7b28b1cdd4c5853b87a5dcd8bef
zhzenghui <zhzenghui@gmail.com>
2014-03-06 13:17:54 +0800

    Finish static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+  
+  def home
+  end
+
+  def help
+  end
+  
+  
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 BigSite::Application.routes.draw do
+  
+  root 'static_pages#index'
+  
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
